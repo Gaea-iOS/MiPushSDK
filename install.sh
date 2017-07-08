d7427219f1e0bc8c1011aeb084fad4590415e461
jerry <445242970@qq.com>
2017-07-08 13:38:23 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 
 #!/bin/sh
-
+echo "start to download MiPushSDK.zip"
 rm -rf MiPush*.zip
-curl -o MiPush.zip http://osr81xpwt.bkt.clouddn.com/MiPush2.2.6.zip
-unzip MiPush.zip
+curl -o MiPushSDK.zip http://osr81xpwt.bkt.clouddn.com/MiPush2.2.6.zip
+echo "start to unzip MiPushSDK.zip"
+unzip MiPushSDK.zip
+echo "unzip MiPushSDK.zip done"
